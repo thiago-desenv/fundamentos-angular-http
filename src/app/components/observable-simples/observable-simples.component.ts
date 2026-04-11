b2f58a49b85b62b12ab3e983326e633fc847d46d
thiago_osim <thiago_osim@hotmail.com>
2026-04-11 15:53:06 -0300

    Criado o componente observable-simples e  o service observable

---
+++ src/app/components/observable-simples/observable-simples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observable-simples',
+  templateUrl: './observable-simples.component.html',
+  styleUrls: ['./observable-simples.component.css']
+})
+export class ObservableSimplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
