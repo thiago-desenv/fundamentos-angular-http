e7696e3aa957cd68ea0597108d18577fa53d9f40
thiago_osim <thiago_osim@hotmail.com>
2026-04-14 23:01:50 -0300

    Criado o componente subject e o service subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ObservableSimplesComponent } from './components/observable-simples/observable-simples.component';
 import { SearchComponent } from './search/search.component';
+import { SubjectComponent } from './components/subject/subject.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ObservableSimplesComponent, SearchComponent],
+  imports: [CommonModule, RouterOutlet, ObservableSimplesComponent, SearchComponent, SubjectComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
