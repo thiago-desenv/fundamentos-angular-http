889c3c4539e77c88ac7539080a7cbd6906d1ceaa
thiago_osim <thiago_osim@hotmail.com>
2026-04-16 22:28:50 -0300

    Criado  os métodos valueChanged e emitValue em subject.service

--- src/app/components/subject/subject.service.ts
+++ src/app/components/subject/subject.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectService {
+  private readonly valueChanged$ = new Subject<number>();
+  // readonly valueChanged$ = new BehaviorSubject<number>(1);
 
-constructor() { }
+  valueChanged(): Observable<number> {
+    return this.valueChanged$.asObservable();
+  }
 
+  emitValue(num: number) {
+    this.valueChanged$.next(num);
+  }
 }
